3093caa403031065962ebe2beef2fb6613867284
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-09-10 12:26:51 +0000

    comparison

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -7,15 +7,26 @@
 
 // console.log("2" > 1);
 // console.log("02" > 1);
+// Output of the above logs:
+// true
+// true
 
 // Avoid this conversoin:
 // console.log(null > 0);
 // console.log(null == 0);
 // console.log(null >= 1);
+// Output of the above logs:
+// false
+// false
+// true
 
 console.log(undefined > 0);
 console.log(undefined == 0);
 console.log(undefined >= 1);
+// Output of the above logs:
+// false
+// false
+// false
 
 // Strict Check: 
 // ===
