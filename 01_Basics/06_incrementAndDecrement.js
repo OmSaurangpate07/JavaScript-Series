4f6968422f30c84e32b9cfb84a5b7953caaa106f
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-09-14 09:16:59 +0000

    Increment and decrement operator

---
+++ 01_Basics/06_incrementAndDecrement.js
@@ -0,0 +1,15 @@
+let x = 3;
+const y = x++;
+console.log(`${x}, ${y}`); // Output: 4, 3
+
+// let x = 3;
+// const y = ++x;
+// console.log(`${x}, ${y}`); // Output: 4, 4
+
+// let x = 3;
+// const y = x--;
+// console.log(`${x}, ${y}`); // Output: 2, 3
+
+// let x = 3;
+// const y = --x;
+// console.log(`${x}, ${y}`); // Output: 2, 2
