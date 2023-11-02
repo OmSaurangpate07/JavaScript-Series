8f02ee42b4a55dca8acc6141a74e20bacffc54c0
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-11-02 12:43:21 +0000

    change in variable file

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,17 +1,17 @@
-const accountId = 144553
-let accountEmail = "om@dev.com"
-var accountPassword = "12345"
-accountCity = "Pune"
-let accountState
+const accountId = 144553;
+let accountEmail = "om@dev.com";
+var accountPassword = "12345";
+accountCity = "Pune";
+let accountState;
 
 // accountId = 4; // not allowed
 // console.log(accountId);
 
-accountEmail = "hitesh@lco.com"
-accountPassword = "21214"
-accountCity = "Jaipur"
+accountEmail = "hitesh@lco.com";
+accountPassword = "21214";
+accountCity = "Jaipur";
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
 /*
 Prefer not to use var,
